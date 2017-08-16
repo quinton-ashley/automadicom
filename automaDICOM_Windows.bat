ad5a77f58bf4580320a5153c8ef7828fe897118e
Quinton Ashley <quintonashley5@gmail.com>
2017-08-16 16:57:15 -0700

    0.5.2

--- automaDICOM_Windows.bat
+++ automaDICOM_Windows.bat
@@ -1,1 +1,1 @@
-start "" "%HOMEPATH%/AppData/Local/Programs/Git/git-bash.exe" -i -l -c "bash -c 'npm start; exec bash'"
+start "" "%SYSTEMDRIVE%/Program Files/Git/git-bash.exe" -i -l -c "bash -c 'npm start; exec bash'"
