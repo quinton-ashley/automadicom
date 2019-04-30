1fd32b55b1061ebf7dc0f2995327b7c7f1378898
Quinton Ashley <qashto@gmail.com>
2019-04-30 13:45:27 -0400

    0.5.34

--- core/dcmtk.js
+++ core/dcmtk.js
@@ -3,7 +3,7 @@
  * Open Source MIT Licensed
  */
 
-const child = require('child_process').spawn;
+const child = require('child_process').execFile;
 const dcm2json = `${__rootDir}/dcmtk/${osType}/dcm2json${((win)?'.exe':'')}`;
 const dcmodify = `${__rootDir}/dcmtk/${osType}/dcmodify${((win)?'.exe':'')}`;
 
@@ -17,58 +17,62 @@ class DCMTK {
 
 	async getTags(file) {
 		return new Promise((resolve, reject) => {
-			let cmd = child(dcm2json, [file, '-fc']);
 			let str = '';
 			let prevData = '';
+			let kill = false;
+			let finished = false;
 
 			let filterResults = () => {
+				if (finished) return;
 				let tags;
+				if (str.slice(-1) != '}') str += '"}}';
 				try {
 					tags = parseJSON(str);
-				} catch (ror) {}
+				} catch (ror) {
+					er(ror);
+				}
 				for (let id in tags) {
 					if (tags[id].InlineBinary) {
 						delete tags[id];
 					}
 				}
+				log(tags);
+				finished = true;
 				resolve(tags);
 			};
 
+			let cmd = child(dcm2json, [file, '-fc'], filterResults);
+
 			cmd.stdout.on('data', (data) => {
+				if (kill) return;
 				data = data.toString();
-				if ((prevData + data).match(/,\s*"7fe00010"/)) {
+				if ((prevData + data).match(/InlineBinary/)) {
 					str += data;
-					str = str.split(/,\s*"7fe00010"/)[0];
+					str = str.split(/,[^,]*,"InlineBinary/)[0];
 					str += '}';
 					cmd.kill('SIGINT');
+					kill = true;
 					filterResults();
 				} else {
 					str += data;
 				}
 				prevData = data;
 			});
-
-			cmd.on('end', filterResults);
-
-			cmd.on('error', (err, item) => reject(err, item));
 		});
 	}
 
 	async modifyTags(file, values) {
-		let args = [file];
-		args.push('--ignore-missing-tags');
-		args.push('--ignore-errors');
-		for (let tagName in values) {
-			args.push('-ma');
-			args.push(`${tagName}=${values[tagName]}`);
-		}
-		log(args);
-		try {
-			await spawn(dcmodify, args, {
-				stdio: 'inherit'
-			});
-		} catch (ror) {}
-		return file + '.bak';
+		return new Promise((resolve, reject) => {
+			let args = [file];
+			args.push('--ignore-missing-tags');
+			args.push('--ignore-errors');
+			for (let tagName in values) {
+				args.push('-ma');
+				args.push(`${tagName}=${values[tagName]}`);
+			}
+			log(args);
+			let cmd = child(dcmodify, args, () => resolve(file + '.bak'));
+		});
 	}
 }
 
